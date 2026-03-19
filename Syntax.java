10d7ee5b5f0617331e381e1211a314ba9b84675e
Pallavi Sharma <146058395+SirfPallavi@users.noreply.github.com>
2026-03-19 22:25:16 +0530

    Add Syntax.java to print a name multiple times

---
+++ Syntax.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; 
+public class name {
+    public static void main(String[] args) {
+        System.out.print("Pallavi Sharma\n" );
+        System.out.print("Pallavi Sharma");
+        System.out.print("Pallavi Sharma" );
+    }
+}
